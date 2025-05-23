9fda36726e979c1bb93a054c7f8bdd8c12af471c
SaltySalt77 <hyna0707@gmail.com>
2025-05-23 12:29:24 +0900

    Feat: 로그인 안 되었을 시 로그인 화면 이동

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
-
     return MaterialApp.router(
       routerConfig: router,
       title: 'Flutter Demo',
