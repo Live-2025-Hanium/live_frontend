4539795ff71efdf427d3e39f0c9627a80fe3daf9
Hyeon Na <alexisna77@gmail.com>
2025-05-24 22:00:02 +0900

    Design: 반응형 디자인 처리 (#10)
    - 디자인 시스템 처리

---
+++ lib/screens/login/widgets/login_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:live_frontend/theme/app_text_styles.dart';
+
+class LoginButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String label;
+  final Widget icon;
+  final Color? backgroundColor;
+  final BorderSide borderSide;
+
+  const LoginButton({
+    super.key,
+    required this.onPressed,
+    required this.label,
+    required this.icon,
+    this.backgroundColor,
+    this.borderSide = BorderSide.none,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 48,
+      child: TextButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: borderSide,
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(alignment: Alignment.centerLeft, child: icon),
+            Center(
+              child: Text(
+                label,
+                style: AppTextStyles.bodyMedium(context, color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
