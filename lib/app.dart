e0ffbbb1d4a48c88be25007627763a56a4aa78d3
LBB <163499519+202780@users.noreply.github.com>
2025-05-24 14:06:52 +0900

    Feat: firebase 인증 설정 및 Google 로그인 기능 기본 연동 (iOS/Android) (#9)
    * Feat: iOS용 Firebase Auth 및 구글 로그인 패키지 의존성 추가
    
    * Chore: iOS에서 Firebase 인증을 위한 GoogleService-Info.plist 추가 후 gitignore 수정
    
    * Chore: FlutterFire CLI를 통한 Firebase 설정 파일 추가 및 프로젝트 연동
    
    * Chore: gitignore에 안드로이드에서 Firebase 사용 위한 google-services.json 추가
    
    * Refactor: main.dart와 app.dart 분리 및 Firebase 초기화 코드 추가
    
    * Chore: Firebase 호환 문제로 iOS 최소 버전 14.0로 상향
    
    * Feat: Pretendard 폰트 파일 3종 추가
    
    * Fix: Firebase Swift Package 의존성 제거 및 Xcode 설정 정리
    
    ---------
    
    Co-authored-by: 이보배 <a82109@Mac.kornet>

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'routers/app_router.dart';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'routers/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(routerProvider);
-    return MaterialApp.router(
-      routerConfig: router,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-    );
-  }
 }
