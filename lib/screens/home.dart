1ffda699b6725e88cbc03d8f3efa33f56d3b8c34
SaltySalt77 <hyna0707@gmail.com>
2025-07-01 16:15:13 +0900

    Feat: 토스트 알림 ui 추가

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:live_frontend/widgets/saeip_app_bar.dart';
 import 'package:live_frontend/widgets/saeip_button.dart';
 import 'package:live_frontend/widgets/saeip_modal.dart';
+import 'package:live_frontend/widgets/saeip_toast.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,11 +11,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SaeipAppBar(title: 'Home'),
-      body: Center(
-        child: SaeipButton.outlined(
-          onPressed: () => _dialogBuilder(context),
-          text: '모달 띄우기',
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            child: SaeipButton.outlined(
+              onPressed: () => _dialogBuilder(context),
+              text: '모달 띄우기',
+            ),
+          ),
+          SaeipToast(message: '토스트 메시지 테스트'),
+        ],
       ),
     );
   }
