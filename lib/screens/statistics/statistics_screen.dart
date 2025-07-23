8d14b9660de48f7e4af597de58074c6c5e28e55e
SaltySalt77 <hyna0707@gmail.com>
2025-07-24 00:34:58 +0900

    Feat: 클로버 미션 추가 UI 구현 중

--- lib/screens/statistics/statistics_screen.dart
+++ lib/screens/statistics/statistics_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:live_frontend/theme/app_colors.dart';
+import 'package:live_frontend/theme/app_text_styles.dart';
 import 'package:live_frontend/widgets/saeip_app_bar.dart';
 import 'package:live_frontend/widgets/saeip_navigation_bar.dart';
 
@@ -9,7 +13,53 @@ class StatisticsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SaeipAppBar(title: 'Statistics'),
-      body: const Center(child: Text('통계 화면')),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        color: Colors.grey[200],
+        child: Container(
+          height: 80.h,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: Colors.white,
+            border: Border.all(color: AppColors.blackBlack1, width: 1.0),
+          ),
+          child: Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 22.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '1',
+                  style: AppTextStyles.titleMedium(
+                    context,
+                    color: AppColors.blackBlack4,
+                  ),
+                ),
+                Gap(24.w),
+                Column(
+                  children: [
+                    Text(
+                      'Total Views',
+                      style: AppTextStyles.bodyRegular(
+                        context,
+                        color: AppColors.blackBlack4,
+                      ),
+                    ),
+                    Text(
+                      '1,234',
+                      style: AppTextStyles.bodyRegular(
+                        context,
+                        color: AppColors.blackBlack4,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       bottomNavigationBar: const SaeipNavigationBar(initialIndex: 1),
     );
   }
