fda465f0d62067e45983e3c73d28c04b94fffe02
LBB <202780@jnu.ac.kr>
2025-08-28 07:13:11 +0900

    Chore: kakao_map_plugin 패키지 설치 후 Android/iOS 기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:live_frontend/env.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,5 +15,9 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   KakaoSdk.init(nativeAppKey: Env.kakaoNativeAppKey);
   await Jiffy.setLocale('ko');
+  AuthRepository.initialize(
+    appKey: dotenv.env['KAKAO_JS_APP_KEY'] ?? 'default_app_key',
+    baseUrl: 'http://localhost',
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:live_frontend/widgets/saeip_app_bar.dart';
 import 'package:live_frontend/widgets/saeip_navigation_bar.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
@@ -8,13 +8,8 @@ class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SaeipAppBar(title: 'Map'),
-      body: Center(
-        child: Text(
-          'Map Screen',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
+      
+      body: KakaoMap(),
       bottomNavigationBar: const SaeipNavigationBar(initialIndex: 2),
     );
   }
