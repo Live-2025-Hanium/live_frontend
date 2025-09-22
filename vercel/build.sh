bda3e007d9bc4247d59aa64e5ec958facf3fb826
SaltySalt77 <hyna0707@gmail.com>
2025-09-22 15:40:30 +0900

    Chore: build.sh에서 필요 없는 옵션 제거

--- vercel/build.sh
+++ vercel/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 export PATH="$PWD/flutter/bin:$PATH"
-flutter build web --release --wasm=false --web-renderer=html \
+flutter build web --release \
   --dart-define-from-file=.vercel/defines.json
 chmod +x vercel/install.sh vercel/build.sh
