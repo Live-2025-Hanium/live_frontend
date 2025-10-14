ed5a50714b494f07d319f7565bf43d8b325dd84f
SaltySalt77 <hyna0707@gmail.com>
2025-10-14 20:56:12 +0900

    Feat: 주간 통계 및 클로버 미션 성장률 모델 생성

--- lib/models/statistics_model.g.dart
+++ lib/models/statistics_model.g.dart
@@ -21,3 +21,83 @@ Map<String, dynamic> _$MonthlyCompletionRateModelToJson(
   'totalCompleted': instance.totalCompleted,
   'completionRate': instance.completionRate,
 };
+
+WeeklyMissionSummaryModel _$WeeklyMissionSummaryModelFromJson(
+  Map<String, dynamic> json,
+) => WeeklyMissionSummaryModel(
+  weekStartDate: json['weekStartDate'] as String,
+  weekEndDate: json['weekEndDate'] as String,
+  weeklySummary: (json['weeklySummary'] as List<dynamic>)
+      .map((e) => DailyMissionSummary.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
+
+Map<String, dynamic> _$WeeklyMissionSummaryModelToJson(
+  WeeklyMissionSummaryModel instance,
+) => <String, dynamic>{
+  'weekStartDate': instance.weekStartDate,
+  'weekEndDate': instance.weekEndDate,
+  'weeklySummary': instance.weeklySummary,
+};
+
+DailyMissionSummary _$DailyMissionSummaryFromJson(Map<String, dynamic> json) =>
+    DailyMissionSummary(
+      date: json['date'] as String,
+      dayOfWeek: const DayOfWeekConverter().fromJson(
+        json['dayOfWeek'] as String,
+      ),
+      missionCount: (json['missionCount'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$DailyMissionSummaryToJson(
+  DailyMissionSummary instance,
+) => <String, dynamic>{
+  'date': instance.date,
+  'dayOfWeek': const DayOfWeekConverter().toJson(instance.dayOfWeek),
+  'missionCount': instance.missionCount,
+};
+
+MonthlyGrowthModel _$MonthlyGrowthModelFromJson(Map<String, dynamic> json) =>
+    MonthlyGrowthModel(
+      previousMonth: (json['previousMonth'] as num).toInt(),
+      currentMonth: (json['currentMonth'] as num).toInt(),
+      growthSummary: (json['growthSummary'] as List<dynamic>)
+          .map((e) => GrowthSummary.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$MonthlyGrowthModelToJson(MonthlyGrowthModel instance) =>
+    <String, dynamic>{
+      'previousMonth': instance.previousMonth,
+      'currentMonth': instance.currentMonth,
+      'growthSummary': instance.growthSummary,
+    };
+
+GrowthSummary _$GrowthSummaryFromJson(Map<String, dynamic> json) =>
+    GrowthSummary(
+      rank: (json['rank'] as num).toInt(),
+      categoryName: $enumDecode(
+        _$CloverMissionCategoryEnumMap,
+        json['categoryName'],
+      ),
+      previousMonthCount: (json['previousMonthCount'] as num).toInt(),
+      currentMonthCount: (json['currentMonthCount'] as num).toInt(),
+      growthPercentage: (json['growthPercentage'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$GrowthSummaryToJson(GrowthSummary instance) =>
+    <String, dynamic>{
+      'rank': instance.rank,
+      'categoryName': _$CloverMissionCategoryEnumMap[instance.categoryName]!,
+      'previousMonthCount': instance.previousMonthCount,
+      'currentMonthCount': instance.currentMonthCount,
+      'growthPercentage': instance.growthPercentage,
+    };
+
+const _$CloverMissionCategoryEnumMap = {
+  CloverMissionCategory.relationship: 'RELATIONSHIP',
+  CloverMissionCategory.health: 'HEALTH',
+  CloverMissionCategory.work: 'WORK',
+  CloverMissionCategory.hobby: 'HOBBY',
+  CloverMissionCategory.study: 'STUDY',
+};
